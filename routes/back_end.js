2e3c2e4f641370b7decf4e588a3bbe05be4c86fb
Ashley <ashley.spellman@me.com>
2017-12-14 13:01:14 -0800

    MESS

--- routes/back_end.js
+++ routes/back_end.js
@@ -5,4 +5,8 @@ var application_controller = require("../controllers/back_end_controller");
 
 router.post("/upload", application_controller.index);
 
+router.get('/', function (req, res) {
+    res.render('login.html')
+})
+
 module.exports = router;
